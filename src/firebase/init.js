2acdbb3afc70ecc7a849325baa4e6c36a0ab3137
Dylan Turner <dylan@doxy.me>
2019-07-21 19:04:54 -0700

    reformat based on linter

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,13 +3,13 @@ import 'firebase/auth'
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-    apiKey: "AIzaSyA_Xf231urENB0pdwiFRAW3lrdsrcAG9Rs",
-    authDomain: "my-bento-starter.firebaseapp.com",
-    databaseURL: "https://my-bento-starter.firebaseio.com",
-    projectId: "my-bento-starter",
-    storageBucket: "",
-    messagingSenderId: "868412378509",
-    appId: "1:868412378509:web:1852762919320c54"
+  apiKey: 'AIzaSyA_Xf231urENB0pdwiFRAW3lrdsrcAG9Rs',
+  authDomain: 'my-bento-starter.firebaseapp.com',
+  databaseURL: 'https://my-bento-starter.firebaseio.com',
+  projectId: 'my-bento-starter',
+  storageBucket: '',
+  messagingSenderId: '868412378509',
+  appId: '1:868412378509:web:1852762919320c54'
 }
 
 firebase.initializeApp(config)
